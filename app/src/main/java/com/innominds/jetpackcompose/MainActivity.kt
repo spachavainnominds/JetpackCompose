fd69e944dfd5ced60fe2336f9c951e48cf97ac8d
SudhakarPachava <spachava@innominds.com>
2024-02-08 16:07:41 +0530

    Title: Button. Text field, Text components Description: UI component code implementation

---
+++ app/src/main/java/com/innominds/jetpackcompose/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.innominds.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.innominds.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTheme {
+//        Greeting("Android")
+        val tfState = remember { mutableStateOf("") }
+        Column(
+//            horizontalAlignment =  Alignment.Horizontal
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 20.dp,
+
+                        ).wrapContentSize()
+            )
+
+            Button(
+//                shape = ,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 20.dp,
+
+                        )
+                    .wrapContentSize(),
+                onClick = { /*TODO*/ }) {
+
+                Text(text = stringResource(id = R.string.app_name))
+            }
+
+            TextField(
+                value = tfState.value,
+                onValueChange = {
+                    tfState.value = it
+                },
+                label = { Text(text = "Enter Label") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 20.dp,
+
+                        ).wrapContentSize()
+//                    .border(2.dp, MaterialTheme.colors.secondary, shape)
+//                    .background(MaterialTheme.colors.primary, shape)
+            )
+
+        }
+
+    }
+}
