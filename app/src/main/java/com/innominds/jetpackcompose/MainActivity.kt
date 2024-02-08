d870a5bb2d5fde59e497bccbc798ba283b66beb4
SudhakarPachava <spachava@innominds.com>
2024-02-08 19:38:22 +0530

    Title: Button. Text field, Text components Description: Font change

--- app/src/main/java/com/innominds/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/innominds/jetpackcompose/MainActivity.kt
@@ -19,8 +19,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.Typeface
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.innominds.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,9 +56,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+val appFontsFamily = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold)
+)
+//    Font(R.font.firasans_italic, FontWeight.Normal, FontStyle.Italic),
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
+
+    /*val fontFamily = remember {
+        FontFamily(
+            typeface = Typeface.createFromAsset(,"font/Regular.otf")
+        )
+    }*/
+
     JetpackComposeTheme {
 //        Greeting("Android")
         val tfState = remember { mutableStateOf("") }
@@ -59,7 +83,11 @@ fun GreetingPreview() {
 //            horizontalAlignment =  Alignment.Horizontal
         ) {
             Text(
+                fontSize = 20.sp,
+                fontFamily = appFontsFamily,
+                fontWeight = FontWeight.Bold,
                 text = stringResource(id = R.string.app_name),
+//                fontSize = 18,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(
@@ -86,15 +114,24 @@ fun GreetingPreview() {
                     .wrapContentSize(),
                 onClick = { /*TODO*/ }) {
 
-                Text(text = stringResource(id = R.string.app_name))
+                Text(text = stringResource(id = R.string.app_name),
+                    fontSize = 16.sp,
+                    fontFamily = appFontsFamily,
+                    fontWeight = FontWeight.Bold,
+                    )
             }
 
             TextField(
+                textStyle = TextStyle(
+                    fontFamily = appFontsFamily,
+                    fontWeight = FontWeight.Bold,
+                ),
                 value = tfState.value,
                 onValueChange = {
                     tfState.value = it
                 },
                 label = { Text(text = "Enter Label") },
+
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(
