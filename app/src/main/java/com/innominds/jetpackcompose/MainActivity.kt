36f6c4fa3a82ccc9113eaec7774678c4c74b743e
SudhakarPachava <spachava@innominds.com>
2024-02-08 16:49:58 +0530

    Title: Button. Text field, Text components Description: UI component alignments

--- app/src/main/java/com/innominds/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/innominds/jetpackcompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.innominds.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +16,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,6 +55,7 @@ fun GreetingPreview() {
 //        Greeting("Android")
         val tfState = remember { mutableStateOf("") }
         Column(
+            modifier = Modifier.fillMaxSize()
 //            horizontalAlignment =  Alignment.Horizontal
         ) {
             Text(
@@ -66,7 +67,10 @@ fun GreetingPreview() {
                         end = 20.dp,
                         top = 20.dp,
 
-                        ).wrapContentSize()
+                        )
+                    .wrapContentSize(
+                        align = Alignment.TopStart
+                    )
             )
 
             Button(
@@ -98,9 +102,10 @@ fun GreetingPreview() {
                         end = 20.dp,
                         top = 20.dp,
 
-                        ).wrapContentSize()
-//                    .border(2.dp, MaterialTheme.colors.secondary, shape)
-//                    .background(MaterialTheme.colors.primary, shape)
+                        )
+                    .wrapContentSize(
+                        align = Alignment.TopStart
+                    )
             )
 
         }
